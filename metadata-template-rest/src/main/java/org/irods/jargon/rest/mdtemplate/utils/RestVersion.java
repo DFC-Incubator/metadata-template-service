b47910ed5da0b13e5a1252f07c37a996cb2f0209
michael-conway <michael_conway@unc.edu>
2017-04-11 15:19:47 -0400

    #1 fix build

--- metadata-template-rest/src/main/java/org/irods/jargon/rest/mdtemplate/utils/RestVersion.java
+++ metadata-template-rest/src/main/java/org/irods/jargon/rest/mdtemplate/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.rest.mdtemplate.utils;
 public final class RestVersion {
  public static String VERSION="4.2.1.1-SNAPSHOT";
- public static String BUILD_TIME="2017-04-04T14:55:48Z";
+ public static String BUILD_TIME="2017-04-11T19:19:18Z";
 }
